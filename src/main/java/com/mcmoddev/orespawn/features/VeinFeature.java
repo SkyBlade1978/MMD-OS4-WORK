7a345f7290d9977e55981ffeb6baa7956f79317d
Daniel Hazelton <dshadowwolf@gmail.com>
2024-08-20 09:42:05 -0400

    fix some style issues and other nits

--- src/main/java/com/mcmoddev/orespawn/features/VeinFeature.java
+++ src/main/java/com/mcmoddev/orespawn/features/VeinFeature.java
@@ -5,18 +5,15 @@ import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.chunk.BulkSectionAccess;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import org.apache.commons.lang3.tuple.Pair;
 
-import java.math.BigInteger;
-import java.security.NoSuchAlgorithmException;
-import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VeinFeature extends Feature<VeinConfiguration> {
+    private static final String eMess = "Value %d out of range (1-6)";
     public VeinFeature(Codec<VeinConfiguration> pCodec) { super(pCodec); }
 
     @Override
@@ -30,9 +27,9 @@ public class VeinFeature extends Feature<VeinConfiguration> {
         if (randomsource.nextFloat() <= veinconfiguration.frequency / (veinconfiguration.frequency>1?100:1)) {
             float base = randomsource.nextFloat() * (float) Math.PI;
             float sizeMod = veinconfiguration.size / 4.0f;
-            double startX = (double)blockpos.getX() * Math.sin((double)f) * (double)sizeMod;
+            double startX = (double)blockpos.getX() * Math.sin((double)base) * (double)sizeMod;
             double startY = (double)(blockpos.getY() * randomsource.nextInt(3) - 2);
-            double startZ = (double)blockpos.getX() * Math.sin((double)f) * (double)sizeMod;
+            double startZ = (double)blockpos.getX() * Math.sin((double)base) * (double)sizeMod;
             int length = randomsource.nextInt(veinconfiguration.minLength, veinconfiguration.maxLength);
             return doPlacement(worldgenlevel, randomsource, veinconfiguration, startX, startY, startZ, length);
         }
@@ -44,42 +41,42 @@ public class VeinFeature extends Feature<VeinConfiguration> {
         int cn = 0;
         int ls = -1;
         int ns = -1;
-        BlockPos curpos = new BlockPos(startX, startY, startZ);
+        BlockPos curpos = new BlockPos((int) startX, (int) startY, (int) startZ);
         List<Pair<BlockPos, Integer>> seen = new ArrayList<>();
         while(cn <= length) {
             while( ns == ls )
                 ns = pRandom.nextInt(1, 6);
-            cn += 1;
-            seen.add(Pair.of(curpos, ls==-1?ns:ls));
-            switch(ns) {
-                case 1:
-                    curpos = curpos.above();
-                    break;
-                case 2:
-                    curpos = curpos.below();
-                    break;
-                case 3:
-                    curpos = curpos.east();
-                    break;
-                case 4:
-                    curpos = curpos.west();
-                    break;
-                case 5:
-                    curpos = curpos.north();
-                    break;
-                case 6:
-                    curpos = curpos.south();
-                    break;
-                default:
-                    throw new RuntimeException(String.format("Value %i out of range (1-6)", ns));
-            }
-            if (seen.contains(curpos)) cn = length+1; // early exit, we've crossed ourself
+            if (ls > -1) seen.add(Pair.of(invert(curpos, ls), ls));
+            curpos = switch (ns) {
+                case 1 -> curpos.above();
+                case 2 -> curpos.below();
+                case 3 -> curpos.east();
+                case 4 -> curpos.west();
+                case 5 -> curpos.north();
+                case 6 -> curpos.south();
+                default -> throw new RuntimeException(String.format(eMess, ns));
+            };
+            if (seen.contains(curpos)) cn = length+1; // early exit, we've crossed our previous track
             ls = ns;
+            cn += 1;
         }
-        seen.stream().forEach(bp -> makeNodeAt(pLevel, pConfig, bp));
+        seen.add(Pair.of(curpos, ns));
+        seen.forEach(bp -> makeNodeAt(pLevel, pConfig, bp));
         return false;
     }
 
+    private BlockPos invert(BlockPos pPos, int previous) {
+        return switch (previous) {
+            case 1 -> pPos.below();
+            case 2 -> pPos.above();
+            case 3 -> pPos.west();
+            case 4 -> pPos.east();
+            case 5 -> pPos.south();
+            case 6 -> pPos.north();
+            default -> throw new RuntimeException(String.format(eMess, previous));
+        };
+
+    }
     private void makeNodeAt(WorldGenLevel pLevel, VeinConfiguration pConfig, Pair<BlockPos, Integer> pLoc) {
         // the right side of each loc determines if the node generates vertical or horizontal
         switch(pLoc.getRight()) {
@@ -92,7 +89,7 @@ public class VeinFeature extends Feature<VeinConfiguration> {
             case 6:
                 makeVertical(pLoc.getLeft(), pConfig);
             default:
-                throw new RuntimeException(String.format("Value %i out of range (1-6)", ns));
+                throw new RuntimeException(String.format(eMess, pLoc.getRight()));
         }
     }
 
