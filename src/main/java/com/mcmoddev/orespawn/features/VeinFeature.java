d4ccbc2a1d5da8d222a244c7d5459ad5258f30f0
Daniel Hazelton <dshadowwolf@gmail.com>
2024-09-06 11:09:54 -0400

    drop another warning out

--- src/main/java/com/mcmoddev/orespawn/features/VeinFeature.java
+++ src/main/java/com/mcmoddev/orespawn/features/VeinFeature.java
@@ -1,7 +1,6 @@
 package com.mcmoddev.orespawn.features;
 
 import com.mcmoddev.orespawn.features.configs.VeinConfiguration;
-import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
