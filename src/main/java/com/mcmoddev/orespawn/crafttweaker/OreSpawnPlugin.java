a39167b0787678adcb5de4124d1edb0e7b4a8afc
Daniel Hazelton <dshadowwolf@gmail.com>
2024-09-13 22:27:53 -0400

    Start on the CraftTweaker integration

---
+++ src/main/java/com/mcmoddev/orespawn/crafttweaker/OreSpawnPlugin.java
@@ -0,0 +1,17 @@
+package com.mcmoddev.orespawn.crafttweaker;
+import com.blamejared.crafttweaker.api.plugin.CraftTweakerPlugin;
+import com.blamejared.crafttweaker.api.plugin.IBracketParserRegistrationHandler;
+import com.blamejared.crafttweaker.api.plugin.ICommandRegistrationHandler;
+import com.blamejared.crafttweaker.api.plugin.ICraftTweakerPlugin;
+import com.blamejared.crafttweaker.api.plugin.IListenerRegistrationHandler;
+import com.blamejared.crafttweaker.api.plugin.ILoaderRegistrationHandler;
+import com.blamejared.crafttweaker.api.plugin.IScriptLoadSourceRegistrationHandler;
+import com.blamejared.crafttweaker.api.plugin.IScriptRunModuleConfiguratorRegistrationHandler;
+import com.blamejared.crafttweaker.api.zencode.scriptrun.IScriptRunModuleConfigurator;
+public class OreSpawnPlugin implements ICraftTweakerPlugin {
+    @Override
+    public void registerLoaders(final ILoaderRegistrationHandler handler) {}
+
+    @Override
+    public void initialize() {}
+}
